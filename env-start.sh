214410d0e737a6f04ba08568c90074ec1ddc381e
Sean Batzel <romulus108@protonmail.com>
2022-05-26 00:19:57 -0400

    Docker improvements.

--- env-start.sh
+++
@@ -1,8 +0,0 @@
-apt update
-apt install -y unzip curl build-essential protobuf-compiler \
-               clang libclang-dev libclang1 llvm llvm-dev clang-tools
-apt upgrade -y
-
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-rustup default stable
-rustup update stable
